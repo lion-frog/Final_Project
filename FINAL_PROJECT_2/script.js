a54a07cc0409a5035a3f9f203c35d612724b0e3d
Your Name <you@example.com>
2023-06-04 10:01:30 -0400

    final commit

---
+++ FINAL_PROJECT_2/script.js
@@ -0,0 +1,8 @@
+// JavaScript code for adding functionality to the button
+$(document).ready(function () {
+  // Click event handler for the button
+  $("#click-me").click(function () {
+    // Redirect to a specific GIF URL
+    window.location.href = "https://i.gifer.com/oNZ.gif";
+  });
+});
